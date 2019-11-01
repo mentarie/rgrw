892cf29c639039fec581d56e5d8096fbdb9b0489
fadhilahpuspasari <fadhilahpuspasari.48@gmail.com>
2019-11-01 23:34:15 +0700

    push ketujuhbelas Fadhilah

---
+++ app/src/main/java/proyek/android/regrow/Model/Activity_donasisaya_donasi.java
@@ -0,0 +1,14 @@
+package proyek.android.regrow.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_donasisaya_donasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donasisaya_donasi);
+    }
+}
